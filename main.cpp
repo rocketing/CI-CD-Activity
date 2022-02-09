683173ac9d37d7191de56de6c2ad4f3196b93797
Abheek Dhawan <abheekd@protonmail.com>
2022-02-08 21:46:25 -0600

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "Congratulations! If you're seeing this, you've (hopefully) set up a "
+          "CI/CD system which builds this C++ code and releases it!"
+       << endl;
+
+  cout << "Learn more:" << endl
+       << "1. https://github.com/features/actions" << endl
+       << "https://woodpecker-ci.org/"
+       << "https://www.redhat.com/en/topics/devops/what-is-ci-cd" << endl;
+}
